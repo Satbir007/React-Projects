7ce367310a3625daed621d9d66877a7bf600e8b3
Satbir Singh <ribtas007@gmail.com>
2023-01-02 11:47:22 +0530

    added textbar and Add button

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Todos = () => {
+
+  const [Todos, setTodo] = useState("");
+ 
+  const handleChange = (event) =>{
+     setTodo(event.target.value)
+  };
+
+  console.log("todo text", Todos);
+
   return (
-    <div>Todo</div>
+    <div className='addTodos'>
+        <input 
+            type="text" 
+            onChange={(event) => handleChange(event)} 
+            className="todo-input" 
+        />
+        <button className="add-btn">Add</button>
+    </div>
   )
 }
 
 export default Todos
+
