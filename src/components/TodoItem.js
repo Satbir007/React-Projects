67497ade99f8ad089b41b17d2865629cc20b0515
Satbir Singh <ribtas007@gmail.com>
2023-01-03 15:55:36 +0530

    fixed bugs

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,53 +3,80 @@ import { motion } from 'framer-motion';
 import { FaEdit, FaCheckDouble, FaWindowClose } from "react-icons/fa";
 
 const TodoItem = (props) => {
-    const {item, updateTodo, removeTodo, completeTodo} = props;
+  const { item, updateTodo, removeTodo, completeTodo } = props;
 
-    const inputRef = useRef(true);
+  const inputRef = useRef(true);
 
-    const changeFocus = () => {
-      inputRef.current.disabled = false;
-      inputRef.current.focus();
-    };
-  
-    const update = (id, value, event) => {
-      if (event.which === 13) {
-        updateTodo({ id, item: value });
-        inputRef.current.disabled = true;
-      }
-    };
+  const changeFocus = () => {
+    inputRef.current.disabled = false;
+    inputRef.current.focus();
+  };
 
+  const update = (id, value, e) => {
+    if (e.which === 13) {
+      //here 13 is key code for enter key
+      updateTodo({ id, item: value });
+      inputRef.current.disabled = true;
+    }
+  };
   return (
-    <motion.li 
-
-    initial={{x:"150vw",transition:{type:"spring",duration:2}}}
-    animate={{x:0,transition:{type:"spring",duration:2}}}
-     whileHover={{scale:0.9,transition:{type:"spring",duration:0.1}}}
-     exit={{x:"-60vw",scale:[1,0], transition:{duration: 0.5}, backgroundColor: "rgba{255,0,0,1}" } }
-    key={item.id} className="card">
-              <textarea
-                ref={inputRef}
-                disabled={inputRef}
-                defaultValue={item.item}
-                onKeyPress={(event) =>
-                  update(item.id, inputRef.current.value, event)
-                }
-              />
-              <div class="btns">
-
-              <motion.button  whileHover={{scale: 1.4}} whileTap={{scale:0.9}} onClick={() => changeFocus()} style={{color:"blue"}}><FaEdit/></motion.button>
-              {
-                item.completed === false && (<motion.button whileHover={{scale: 1.4}} whileTap={{scale:0.9}} style={{color:"green"}} onClick={() => completeTodo(item.id)}>
-                <FaCheckDouble/>
-                </motion.button>)
-                
-              }
-              
-              < motion.button whileHover={{scale: 1.4}} whileTap={{scale:0.9}} style={{color:"red"}} onClick={() => removeTodo(item.id)}><FaWindowClose/></motion.button>
-              </div>
-              {item.completed && <span className="completed">Done</span>}
-            </motion.li>
+    <motion.li
+      initial={{ x: "150vw", transition: { type: "spring", duration: 2 } }}
+      animate={{ x: 0, transition: { type: "spring", duration: 2 } }}
+      whileHover={{
+        scale: 0.9,
+        transition: { type: "spring", duration: 0.1 },
+      }}
+      exit={{
+        x: "-60vw",
+        scale: [1, 0],
+        transition: { duration: 0.5 },
+        backgroundColor: "rgba(255,0,0,1)",
+      }}
+      key={item.id}
+      className="card"
+    >
+      <textarea
+        ref={inputRef}
+        disabled={inputRef}
+        defaultValue={item.item}
+        onKeyPress={(e) => update(item.id, inputRef.current.value, e)}
+      />
+      <div className="btns">
+        <motion.button
+          whileHover={{ scale: 1.4 }}
+          whileTap={{ scale: 0.9 }}
+          onClick={() => changeFocus()}
+        >
+          {" "}
+          <FaEdit/>{" "}
+        </motion.button>
+        {item.completed === false && (
+          <motion.button
+            whileHover={{ scale: 1.4 }}
+            whileTap={{ scale: 0.9 }}
+            style={{ color: "green" }}
+            onClick={() => completeTodo(item.id)}
+          >
+            <FaCheckDouble/>
+          </motion.button>
+        )}
+        <motion.button
+          whileHover={{ scale: 1.4 }}
+          whileTap={{ scale: 0.9 }}
+          style={{ color: "red" }}
+          onClick={() => removeTodo(item.id)}
+        >
+          {" "}
+          <FaWindowClose/>
+        </motion.button>{" "}
+      </div>
+      {item.completed && <span className="completed">done</span>}
+    </motion.li>
   );
 };
 
-export default TodoItem
+export default TodoItem;
+
+
+
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { addTodos } from "../redux/reducer";
-import {GoPlus} from "react-icons/go";
-import { motion } from 'framer-motion';
-
+import { GoPlus } from "react-icons/go";
+import { motion } from "framer-motion";
 
 const mapStateToProps = (state) => {
   return {
@@ -14,69 +13,49 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     addTodo: (obj) => dispatch(addTodos(obj)),
-    // removeTodo: (id) => dispatch(removeTodos(id)),
-    // updateTodo: (obj) => dispatch(updateTodos(obj)),
-    // completeTodo: (id) => dispatch(completeTodos(id)),
   };
 };
 
 const Todos = (props) => {
   const [todo, setTodo] = useState("");
 
-
-
-
-
-  const handleChange = (event) => {
-    setTodo(event.target.value);
+  const handleChange = (e) => {
+    setTodo(e.target.value);
   };
 
- 
-    const add = (second) => 
-      {
-        if(todo===''){
-          alert("Input is empty");
-        }else{
-          props.addTodo({
-            // define object todo
-            id: Math.floor(Math.random() * 1000),
-            item: todo,
-            completed: false, 
-           })
-           setTodo('');
-        }
-       }
-    
-  
-
- // console.log("todo text", todo);
-
+  const add = () => {
+    if (todo === "") {
+      alert("Input is Empty");
+    } else {
+      props.addTodo({
+        id: Math.floor(Math.random() * 1000),
+        item: todo,
+        completed: false,
+      });
+      setTodo("");
+    }
+  };
+  //console.log("props from store", props);
   return (
-  
     <div className="addTodos">
       <input
         type="text"
-        onChange={(event) => handleChange(event)}
+        onChange={(e) => handleChange(e)}
         className="todo-input"
         value={todo}
       />
+
       <motion.button
-        whileHover={{scale: 1.1}}
-        whileTap={{scale:0.9}}
-
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
         className="add-btn"
-        onClick={ ()  => add() }
+        onClick={() => add()}
       >
-        <GoPlus/>
+        <GoPlus />
       </motion.button>
-      
       <br />
-
-      
     </div>
-    
   );
 };
-
-// connecting this component to redux store using conect
+//we can use connect method to connect this component with redux store
 export default connect(mapStateToProps, mapDispatchToProps)(Todos);
